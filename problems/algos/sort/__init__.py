f14ff20b2000a4134f2036e4da3e10b9cceadf6e
Vinodh Periyasamy <vinodhkps@gmail.com>
2015-03-08 22:05:33 -0400

    insertion sort and merge sort.

---
+++ problems/algos/sort/__init__.py
@@ -0,0 +1,11 @@
+from sort_functions import insertion_sort
+from sort_functions import merge_sort
+
+B = [4,7,3,5,2,1,9,6]
+
+print('Merge Sort :')
+merge_sort(B)
+
+print('Insertion Sort :')
+insertion_sort(B)
+
---
+++ problems/algos/sort/sort_functions.py
@@ -0,0 +1,58 @@
+'''
+Created on Mar 8, 2015
+
+Contains various sort functions
+
+@author: Vinodh Periyasamy
+'''
+
+positive_infinity = float("inf")
+
+def insertion_sort(A):
+    print('insertion_sort incoming Array {}'.format(A))
+    for i in range(1, len(A)):
+        key = A[i];
+        j = i - 1;
+        while j >= 0 and A[j] > key:
+            A[j+1] = A[j]
+            j = j - 1;
+        A[j+1] = key
+        
+    print('insertion_sort sorted Array {}'.format(A))
+    
+
+def merge_sort(A):
+    print('merge_sort incoming Array {}'.format(A))
+    __merge_sort_divide(A,0,(len(A)-1))    
+    print('merge_sort sorted Array {}'.format(A))
+
+def __merge_sort_divide(A, p ,r):
+    if(p < r):
+        q = (p+r)//2
+        __merge_sort_divide(A, p, q)
+        __merge_sort_divide(A, q+1, r)
+        __merge(A, p , q, r)
+        
+def __merge(A,p,q,r):
+    n1 = q - p + 1;
+    n2 = r - q;
+    B = []
+    C = []
+    for i in range(0,n1):
+        B.append(A[p+i])
+    for j in range(0,n2):
+        C.append(A[q+j+1])   
+    B.append(positive_infinity)
+    C.append(positive_infinity)
+    i = 0;
+    j = 0;
+    for k in range(p,r+1):
+        if B[i] <= C[j]:
+            A[k] = B[i]
+            i = i + 1
+        else:
+            A[k] = C[j]
+            j = j+1        
+                        
+                
+        
